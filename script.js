7021d1fd5efa90eb9daaa8bb48f5078a2ec557c0
rhlsingh19 <145037560+rhlsingh19@users.noreply.github.com>
2024-01-04 21:09:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-function shortcut(s1, s2) {
-  // your code here
+function shortcut(str1, str2) {
+    if (!str1 || !str2) {
+        return '';
+    }
+
+    const firstLetterStr1 = str1.charAt(0).toUpperCase();
+    const firstLetterStr2 = str2.charAt(0).toUpperCase();
+
+    return `${firstLetterStr1}${firstLetterStr2}`;
 }
+
+// Examples
+console.log(shortcut("Amnesty", "International")); // Output: 'AI'
+console.log(shortcut("Hello", "world")); // Output: 'Hw'
+console.log(shortcut("", "International")); // Output: ''
+console.log(shortcut("Amnesty", "")); // Output: ''
 
 // Do not change the code below.
 const s1 = prompt("Enter s1:");
